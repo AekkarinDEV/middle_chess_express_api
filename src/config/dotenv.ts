e1fa4fa204dc245622c4443723153bb6ac7b6eb3
AekkarinDEV <ezio4227@gmail.com>
2025-06-26 22:34:43 +0700

    delete it all cause it out of bp

--- src/config/dotenv.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-module.exports = {
-  port: process.env.PORT || 4000,
-  mongoURI: process.env.DB_MONGO,
-};
--- src/routes/user.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { jwtValidate } from "../middlewares/jwt_validate";
-import { getUserById } from "../controllers/user_controller";
-
-const userRouter = Router();
-
-userRouter.get("/id", jwtValidate, getUserById);
-
-export default userRouter;
